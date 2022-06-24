5152d4b67a685c30af4d538d410f08c7264a4867
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2022-06-24 06:01:13 +0530

    Add files via upload

---
+++ screens/Iss.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default class IssLocation extends React.Component {
+    render(){return (
+        <View style={styles.container}>
+          <Text> </Text>
+          
+        </View>
+      );}
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/Meteor.js
@@ -0,0 +1,23 @@
+
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class MeteorScreen extends React.Component {
+    render(){return (
+        <View style={styles.container}>
+          <Text> Meteor Screen ! </Text>
+          
+        </View>
+      );}
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
